8a16d4013c0144b08d695e194e604c997fc67f42
Rui (Lisa) Xie <lisaxie3001@gmail.com>
2020-04-27 21:05:18 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-import { headerComponent } from './header/app.headerComponent';
-import { navComponent } from './nav/app.navComponent';
-import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
-import { footerComponent } from './footer/app.footerComponent';
-import { aboutComponent } from './about/app.aboutComponent';
-import { projectComponent } from './project/app.projectComponent';
-import { contactComponent } from './contact/app.contactComponent';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './main.html',
 })
 export class AppComponent {
-  
+  title = 'my-app';
 }
- 
